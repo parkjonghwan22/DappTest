c4f9595cb73819ac863732f534595074d56fe624
parkjonghwan22 <gch02933@naver.com>
2023-06-13 19:49:46 +0900

    feat: ..1

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { RootLayout } from "@components/layout/layout";
 
 const Home: NextPage = () => {
-  console.log("Hello");
   console.log("Hello2");
   return <RootLayout>{/* <Listing /> */}</RootLayout>;
 };
